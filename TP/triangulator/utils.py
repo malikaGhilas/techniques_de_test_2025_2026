656a96e1dc9c98b6a8c7b250e4d7e90ced4cb196
Malika GHILAS <malika.ghilas57@gmail.com>
2025-11-07 16:00:43 +0100

    Create utils.py

---
+++ TP/triangulator/utils.py
@@ -0,0 +1,11 @@
+"""Utilitaires de sérialisation binaire."""
+
+
+def serialize_pointset(points):
+    """Convertis une liste de points en données binaires."""
+    pass
+
+
+def deserialize_pointset(data):
+    """Convertis des données binaires en liste de points."""
+    pass
