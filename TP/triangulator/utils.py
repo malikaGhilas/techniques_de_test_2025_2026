e4cdfbb911d42777e77a45a76dba01eeaddec193
Malika GHILAS <malika.ghilas57@gmail.com>
2025-12-01 19:32:53 +0100

    Update utils.py

--- TP/triangulator/utils.py
+++ TP/triangulator/utils.py
@@ -1,11 +1,15 @@
-"""Utilitaires de sérialisation binaire."""
+import struct
 
+def serialize_pointset(pointset):
+    """Serialize a PointSet to binary format (float32)."""
+    if not pointset:
+        return b"\x00\x00\x00\x00"  # 4 bytes pour 0 points
 
-def serialize_pointset(points):
-    """Convertis une liste de points en données binaires."""
-    pass
+    num_points = len(pointset)
+    data = num_points.to_bytes(4, byteorder='big')
 
+    for x, y in pointset:
+        data += struct.pack('>f', float(x))  # X en float32 big-endian
+        data += struct.pack('>f', float(y))  # Y en float32 big-endian
 
-def deserialize_pointset(data):
-    """Convertis des données binaires en liste de points."""
-    pass
+    return data
