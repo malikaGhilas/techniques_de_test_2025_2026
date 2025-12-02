696d1b3798a846c60f38bcc0bf7b9752b3062d41
Malika GHILAS <malika.ghilas57@gmail.com>
2025-12-02 10:34:39 +0100

    Update utils.py

--- TP/triangulator/utils.py
+++ TP/triangulator/utils.py
@@ -13,3 +13,23 @@ def serialize_pointset(pointset):
         data += struct.pack('>f', float(y))  # Y en float32 big-endian
 
     return data
+
+def deserialize_pointset(data):
+    """Deserialize binary data to list of (x, y) float tuples."""
+    if len(data) < 4:
+        raise ValueError("Data too short to contain point count")
+    num_points = int.from_bytes(data[:4], byteorder='big')
+    if num_points == 0:
+        return []
+    expected_length = 4 + 8 * num_points
+    if len(data) < expected_length:
+        raise ValueError(f"Data too short: expected {expected_length} bytes, got {len(data)}")
+    
+    points = []
+    offset = 4
+    for _ in range(num_points):
+        x = struct.unpack('>f', data[offset:offset+4])[0]
+        y = struct.unpack('>f', data[offset+4:offset+8])[0]
+        points.append((x, y))
+        offset += 8
+    return points
